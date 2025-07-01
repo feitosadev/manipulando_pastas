abd648287467c3bae860e37e5e7d1bbda41f9754
José Carlos <jccfjr@gmail.com>
2025-06-30 22:12:37 -0300

    Criação da classe principal.

---
+++ src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite o caminho da pasta: ");		
+		String stringPath = entrada.nextLine();
+		
+		File path = new File(stringPath);
+		
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS:");
+		for(File folder: folders) {
+			System.out.println(folder);
+		}
+		
+		System.out.println();
+		
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("FILES:");
+		for(File file: files) {
+			System.out.println(file);
+		}
+		entrada.close();
+	}
+}
